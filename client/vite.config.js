4b3c26653dfb6d789796d547dd7b723512a7afdf
Astrof123 <valeev_101@bk.ru>
2025-05-27 11:20:02 +0800

    Added another settings for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
@@ -12,7 +11,8 @@ export default defineConfig({
     vueDevTools(),
   ],
   server: {
-    host: true
+    host: true,
+    allowedHosts: ['flowermonitoring.up.railway.app']
   },
   resolve: {
     alias: {
